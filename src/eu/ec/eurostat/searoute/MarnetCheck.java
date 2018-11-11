172a57525036c580c475c1d3bf5e5f71df9e6a39
jgaffuri <jgaf@protonmail.com>
2018-11-11 17:45:50 +0100

    a

--- src/eu/ec/eurostat/searoute/MarnetCheck.java
+++ src/eu/ec/eurostat/searoute/MarnetCheck.java
@@ -1,14 +1,31 @@
 package eu.ec.eurostat.searoute;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.geojson.feature.FeatureJSON;
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.GeoJSONUtil;
+
 public class MarnetCheck {
 
 	public static void main(String[] args) {
 		try {
 			System.out.println("Start.");
 
+
 			//load input
-			
-			
+			ArrayList<Feature> fs = GeoJSONUtil.load("resources/marnet/marnet_working.geojson");
+			System.out.println(fs.size());
+
+
+
 			//make noding
 			//remove duplicate network edges - always keep shorter
 			//make network planar
@@ -16,41 +33,14 @@ public class MarnetCheck {
 			//check 180/-180 compatibility
 
 
-			/*
-			//load features from shp file
-			SHPData shpIn = SHPUtil.loadSHP("/home/julien/ctrweb/contraffic-ws/WebContent/resources/shp/marnet2.shp");
-
-			System.out.println(shpIn.fs.size() + " links");
-
-			//eliminate double links
-			ArrayList<SimpleFeature> fsToRemove = new ArrayList<SimpleFeature>();
-			for(int i=0; i<shpIn.fs.size(); i++){
-				SimpleFeature fi = shpIn.fs.get(i);
-				if(fsToRemove.contains(fi)) continue;
-				MultiLineString li = (MultiLineString)fi.getDefaultGeometry();
-				for(int j=i+1; j<shpIn.fs.size(); j++){
-					SimpleFeature fj = shpIn.fs.get(j);
-					MultiLineString lj = (MultiLineString)fj.getDefaultGeometry();
-					if(li.getLength() != lj.getLength()) continue;
-					Geometry inter = li.intersection(lj);
-					if(inter.getLength() != lj.getLength()) continue;
-					fsToRemove.add(fj);
-				}
-			}
-			System.out.println(fsToRemove.size() + " overlapping links to remove.");
-			shpIn.fs.removeAll(fsToRemove);
-			System.out.println("   " + shpIn.fs.size() + " links remaining.");
-
-			//TODO
 			//integrate
 			//DP filter
 			//linemerger
 			//cut lines depending on intersection
 
-			System.out.println("Saving...");
-			SHPUtil.saveSHP(shpIn.ft, shpIn.fs, "/home/julien/ctrweb/contraffic-ws/WebContent/resources/shp/", "marnet_fil.shp");
-			//SHPUtil.saveSHP(lines, 4326, "/home/julien/ctrweb/contraffic-ws/WebContent/resources/shp/", "marnet_fil.shp");
-			 */
+			//save output
+			//TODO test that !
+			//GeoJSONUtil.save(fs, "resources/marnet/marnet_working_out.geojson");
 
 			System.out.println("Done.");
 		} catch (Exception e) { e.printStackTrace(); }
