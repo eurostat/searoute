4170b7b50bd8a5c15a31466f53d495b6d3f31442
jgaffuri <jgaf@protonmail.com>
2018-11-15 23:14:13 +0100

    a

--- src/eu/ec/eurostat/searoute/MarnetCheck.java
+++ src/eu/ec/eurostat/searoute/MarnetCheck.java
@@ -7,6 +7,7 @@ import org.geotools.referencing.crs.DefaultGeographicCRS;
 import org.opencarto.algo.base.Union;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.GeoJSONUtil;
+import org.opencarto.util.JTSGeomUtil;
 
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.LineString;
@@ -20,17 +21,34 @@ public class MarnetCheck {
 
 
 			//load input
-			ArrayList<Feature> fs = GeoJSONUtil.load("resources/marnet/marnet_working.geojson");
+			ArrayList<Feature> fs = GeoJSONUtil.load("resources/marnet/marnet_densified.geojson");
+			System.out.println(fs.size());
 
 			//test linemerger
 			LineMerger lm = new LineMerger();
 			for(Feature f : fs) lm.add(f.getGeom());
 			Collection<Geometry> lines = lm.getMergedLineStrings();
+			System.out.println(lines.size());
 
 			Geometry u = Union.getLineUnion(lines);
 			System.out.println(u.getGeometryType());
-			System.out.println(u.getCoordinates().length);
-			
+			System.out.println(u.getCoordinates().length + " cs");
+			Collection<LineString> lines_ = JTSGeomUtil.getLineStringGeometries(u);
+			System.out.println(lines_.size());
+
+			lm = new LineMerger();
+			lm.add(lines_);
+			lines = lm.getMergedLineStrings();
+			System.out.println(lines.size());
+
+
+			//TODO
+			//DP filter
+			//check number of connex components
+			//remove duplicate network edges - always keep shorter
+			//make noding - integrate
+			//check 180/-180 compatibility
+
 			ArrayList<Feature> fsOut = new ArrayList<Feature>();
 			int i=0;
 			for(Geometry ls : lines) {
@@ -40,23 +58,17 @@ public class MarnetCheck {
 				fsOut.add(f);
 			}
 
-			System.out.println(fs.size());
+			//create graph
+			/*Graph g = GraphBuilder.buildForNetworkFromLinearFeatures(fsOut);
+			Collection<Graph> ccs = GraphConnexComponents.get(g);
+			for(Graph cc : ccs) {
+				System.out.println(" "+cc.getNodes().size());
+			}*/
+
+
 			System.out.println(fsOut.size());
 
-			//make noding
-			//remove duplicate network edges - always keep shorter
-			//make network planar
-			//check number of connex components
-			//check 180/-180 compatibility
-
-
-			//integrate
-			//DP filter
-			//linemerger
-			//cut lines depending on intersection
-
 			//save output
-			//TODO test that !
 			GeoJSONUtil.save(fsOut, "resources/marnet/marnet_working_out.geojson", DefaultGeographicCRS.WGS84);
 
 			System.out.println("Done.");
