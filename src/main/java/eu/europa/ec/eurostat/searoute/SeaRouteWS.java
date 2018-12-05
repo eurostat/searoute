023482cb84ef40142e969ddc2ffe5244fcae5129
jgaffuri <jgaf@protonmail.com>
2018-12-05 16:13:04 +0100

    mavenisation

---
+++ src/main/java/eu/europa/ec/eurostat/searoute/SeaRouteWS.java
@@ -0,0 +1,283 @@
+package eu.europa.ec.eurostat.searoute;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.geotools.graph.structure.Node;
+import org.opencarto.datamodel.Feature;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+import eu.europa.ec.eurostat.searoute.utils.Utils;
+
+public class SeaRouteWS extends HttpServlet {
+	//   /usr/share/tomcat8/bin/catalina.sh start
+	//   /usr/share/tomcat8/bin/catalina.sh stop
+	//   http://localhost:8080/
+	//logs: /var/lib/tomcat8/logs/
+	//http://localhost:8080/searoutews/
+	//http://localhost:8080/searoutews/seaws?ser=rou&opos=5.3,43.3&dpos=121.8,31.2
+
+	//https://javatutorial.net/how-to-deploy-and-undeploy-applications-in-tomcat
+	//https://www.baeldung.com/tomcat-deploy-war
+
+
+	private static final DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static final long serialVersionUID = 5326338791791803741L;
+
+	private static final String ENC_CT = "; charset=utf-8";
+
+	private HashMap<Integer,SeaRouting> srs = new HashMap<>();
+
+	/*/cache
+	private HashMap<String, Object[]> cache;
+	private static final int CACHE_MAX_SIZE = 60000; //TODO choose better the cache size, depending on RAM available
+	private String getFromCache(String oLocid, String dLocid){
+		if(oLocid == null || dLocid == null || "".equals(oLocid) || "".equals(dLocid)) return null;
+		Object[] o = cache.get( oLocid + "_" + dLocid );
+		if(o == null) return null;
+		o[1] = ((int)o[1]) + 1;
+		return (String) o[0];
+	}
+	private void setInCache(String oLocid, String dLocid, String st){
+		if(cache.size() >= CACHE_MAX_SIZE) {
+			//remove the ones too less used, below the median
+
+			//get the limit value
+			int[] a = new int[cache.size()]; int i=0;
+			for(Object[] o : cache.values()) a[i++] = (int)o[1];
+			Arrays.sort(a);
+			int limit = a[ (int)(cache.size()*0.5) ];
+
+			//remove all values below the limit
+			Set<String> toRemove = new HashSet<String>();
+			for(Entry<String,Object[]> e : cache.entrySet())
+				if((int)e.getValue()[1] < limit) toRemove.add(e.getKey());
+			for(String toRemove_ : toRemove) cache.remove(toRemove_);
+		}
+		if(oLocid == null || dLocid == null || st == null || "".equals(oLocid) || "".equals(dLocid) || "".equals(st)) return;
+		cache.put(oLocid + "_" + dLocid, new Object[]{st, 1});
+	}*/
+
+
+
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("---" + this + " started - " + df.format(new Date()));
+		//cache = new HashMap<String, Object[]>();
+		try {
+			for(int resKM : SeaRouting.RESOLUTION_KM)
+				srs.put(resKM, new SeaRouting("webapps/searoutews/resources/marnet/marnet_plus_"+resKM+"KM.shp"));
+		} catch (MalformedURLException e) { e.printStackTrace(); }
+	}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//writer for the response
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+		try {
+			//get service
+			String ser = request.getParameter("ser");
+			if(ser == null || "".equals(ser)) ser = "rou";
+
+			switch (ser) {
+
+			case "rouinfo":
+				response.setContentType("text/html"+ENC_CT);
+				//out.print("<html>");
+				out.print("Service running.");
+
+				/*out.print("Cache size: "+ cache.size() + " of "+CACHE_MAX_SIZE);
+
+				//show cache content
+				ArrayList<String> list = new ArrayList<String>();
+				list.addAll(cache.keySet());
+				Collections.sort(list, new Comparator<String>() {
+					@Override
+					public int compare(String o1, String o2) { return (int)cache.get(o2)[1] - (int)cache.get(o1)[1]; }
+				});
+
+				out.print("<table>");
+				for(String id : list){
+					Object[] o = cache.get(id);
+					out.print("<tr>");
+					out.print("<td>" + o[1] + "</td>");
+					out.print("<td>" + id + "</td>");
+					String st = ((String)o[0]);
+					out.print("<td>" + st.substring(0, Math.min(150, st.length())) + " ...</td>");
+					out.print("</tr>");
+				}
+				out.print("</table>");*/
+
+				//out.print("</html>");
+
+				break;
+			case "rou":
+
+				//resolution
+				int resKM = 20;
+				try { resKM = Integer.parseInt(request.getParameter("res")); } catch (Exception e) {}
+				SeaRouting sr = srs.get(resKM);
+				if(sr==null) sr = srs.get(20);
+
+				//geometry asked
+				boolean geomP = !("0".equals( request.getParameter("g") ));
+
+				//distance asked
+				boolean distP = !("0".equals( request.getParameter("d") ));
+
+				//lat lon
+				double[] oLon=null,oLat=null,dLon=null,dLat=null;
+
+				//load directly position (lon,lat)
+				String oPos = request.getParameter("opos");
+				if(oPos!=null && !"".equals(oPos)){
+					String[] s = oPos.split("\\s*,\\s*");
+					int nb = s.length/2;
+					oLon = new double[nb]; oLat = new double[nb];
+					for(int i=0;i<nb;i++){
+						oLon[i] = Double.parseDouble(s[2*i]);
+						oLat[i] = Double.parseDouble(s[2*i+1]);
+					}
+				}
+				String dPos = request.getParameter("dpos");
+				if(dPos!=null && !"".equals(dPos)){
+					String[] s = dPos.split("\\s*,\\s*");
+					int nb = s.length/2;
+					dLon = new double[nb]; dLat = new double[nb];
+					for(int i=0;i<nb;i++){
+						dLon[i] = Double.parseDouble(s[2*i]);
+						dLat[i] = Double.parseDouble(s[2*i+1]);
+					}
+				}
+
+				response.setContentType("text/json"+ENC_CT);
+				if(oLon==null)
+					out.print("{\"status\":\"empty\"}");
+				if(oLon.length==0)
+					out.print("{\"status\":\"empty\"}");
+				else {
+					if(oLon.length>1) out.print("[");
+					returnRoute(out, oLon[0], oLat[0], dLon[0], dLat[0], distP, geomP, sr);
+					for(int i=1; i<oLon.length; i++){
+						out.print(",");
+						returnRoute(out, oLon[i], oLat[i], dLon[i], dLat[i], distP, geomP, sr);
+					}
+					if(oLon.length>1) out.print("]");
+				}
+				break;
+			default :
+				response.setContentType("text/json"+ENC_CT);
+				out.println("{\"res\":\"error\",\"message\":\"Unknown service: "+ser+"\"}");
+			}
+
+		} catch (Exception e) {
+			response.setContentType("text/"+ENC_CT);
+			e.printStackTrace();
+			e.printStackTrace(out);
+		} finally {
+			//close writer
+			if (out!=null) try {out.close();} catch (Exception e) {}
+		}
+
+	}
+
+	private void returnRoute(PrintWriter out, double oLon, double oLat, double dLon, double dLat, boolean distP, boolean geomP, SeaRouting sr) {
+		try {
+			if(oLon==Double.NaN || oLat==Double.NaN){
+				out.print("{\"status\":\"error\",\"message\":\"Unknown origin location\"");
+				out.print("}");
+				return;
+			}
+			if(dLon==Double.NaN || dLat==Double.NaN){
+				out.print("{\"status\":\"error\",\"message\":\"Unknown destination location\"");
+				out.print("}");
+				return;
+			}
+			if(oLon==dLon && oLat==dLat){
+				out.print("{\"status\":\"empty\"");
+				out.print("}");
+				return;
+			}
+
+			/*/try to find in cache
+			st = getFromCache(oLocid, dLocid);
+			if(st != null){
+				out.print(st);
+				return;
+			}*/
+
+			//get origin node/positions
+			Coordinate oPos = new Coordinate(oLon,oLat);
+			Node oN = sr.getNode(oPos);
+			//get destination node/positions
+			Coordinate dPos = new Coordinate(dLon,dLat);
+			Node dN = sr.getNode(dPos);
+
+			if(oN == null || dN == null){
+				out.print( "{\"status\":\"error\",\"message\":\"Could not find start/end node\"}" );
+				//setInCache(oLocid, dLocid, st);
+				return;
+			}
+
+			//the maritime route geometry
+			Feature f = sr.getRoute(oPos, oN, dPos, dN);
+			Geometry ls = f.getGeom();
+
+			if(ls==null){
+				out.print( "{\"status\":\"error\",\"message\":\"Shortest path not found\"}" );
+				//setInCache(oLocid, dLocid, st);
+				return;
+			}
+
+			String st;
+			st = "{\"status\":\"ok\"";
+			if(distP){
+				double d = Utils.getLengthGeo(ls);
+				d = Utils.round(d, 2);
+				st += ",\"dist\":"+d;
+			}
+			if(geomP){
+				//export as geojson
+				st += ",\"geom\":" + Utils.toGeoJSON(ls);
+			}
+			st += "}";
+
+			out.print(st);
+			//setInCache(oLocid, dLocid, st);
+
+			//improve network / add inland transport / take into account impedance
+
+		} catch (Exception e) {
+			String st = "{\"status\":\"error\",\"message\":\"Unknown error\"";
+			st += "}";
+			out.print(st);
+			//out.print(e.getMessage());
+			//setInCache(oLocid, dLocid, st);
+			e.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		super.destroy();
+		System.out.println("---" + this + " stopped - " + df.format(new Date()));
+	}
+
+}
---
+++ src/main/java/eu/europa/ec/eurostat/searoute/SeaRouting.java
@@ -0,0 +1,235 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.searoute;
+
+import java.io.File;
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.geotools.graph.build.feature.FeatureGraphGenerator;
+import org.geotools.graph.build.line.LineStringGraphGenerator;
+import org.geotools.graph.path.DijkstraShortestPathFinder;
+import org.geotools.graph.path.Path;
+import org.geotools.graph.structure.Edge;
+import org.geotools.graph.structure.Graph;
+import org.geotools.graph.structure.Node;
+import org.geotools.graph.structure.basic.BasicEdge;
+import org.geotools.graph.traverse.standard.DijkstraIterator;
+import org.geotools.graph.traverse.standard.DijkstraIterator.EdgeWeighter;
+import org.opencarto.datamodel.Feature;
+import org.opengis.feature.simple.SimpleFeature;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.operation.linemerge.LineMerger;
+
+import eu.europa.ec.eurostat.searoute.utils.Utils;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class SeaRouting {
+	public static final int[] RESOLUTION_KM = new int[] { 100, 50, 20, 10, 5 };
+
+	private Graph g;
+	private EdgeWeighter weighter;
+
+	public SeaRouting() throws MalformedURLException { this(20); }
+	public SeaRouting(int resKM) throws MalformedURLException { this("resources/marnet/marnet_plus_"+resKM+"KM.shp"); }
+	public SeaRouting(String path) throws MalformedURLException { this(new File(path)); }
+	public SeaRouting(File marnetFile) throws MalformedURLException { this(marnetFile.toURI().toURL()); }
+	public SeaRouting(URL marnetFileURL) {
+		try {
+			Map<String, Serializable> map = new HashMap<>();
+			map.put( "url", marnetFileURL );
+			DataStore store = DataStoreFinder.getDataStore(map);
+			FeatureCollection fc =  store.getFeatureSource(store.getTypeNames()[0]).getFeatures();
+			store.dispose();
+
+			/*InputStream input = marnetFileURL.openStream();
+			FeatureCollection fc = new FeatureJSON().readFeatureCollection(input);
+			input.close();*/
+
+			//build graph
+			FeatureIterator<?> it = fc.features();
+			FeatureGraphGenerator gGen = new FeatureGraphGenerator(new LineStringGraphGenerator());
+			while(it.hasNext()) gGen.add(it.next());
+			g = gGen.getGraph();
+			it.close();
+		} catch (Exception e) { e.printStackTrace(); }
+
+		//link nodes around the globe
+		for(Object o : g.getNodes()){
+			Node n = (Node)o;
+			Coordinate c = ((Point)n.getObject()).getCoordinate();
+			if(c.x==180) {
+				Node n_ = getNode(new Coordinate(-c.x,c.y));
+				//System.out.println(c + " -> " + ((Point)n_.getObject()).getCoordinate());
+				BasicEdge be = new BasicEdge(n, n_);
+				n.getEdges().add(be);
+				n_.getEdges().add(be);
+				g.getEdges().add(be);
+			}
+		}
+
+		//define weighter
+		weighter = new DijkstraIterator.EdgeWeighter() {
+			public double getWeight(Edge e) {
+				//edge around the globe
+				if( e.getObject()==null ) return 0;
+				SimpleFeature f = (SimpleFeature) e.getObject();
+				return Utils.getLengthGeo((Geometry)f.getDefaultGeometry());
+			}
+		};
+
+	}
+
+
+
+	private Path getShortestPath(Graph g, Node sN, Node dN, EdgeWeighter weighter){
+		DijkstraShortestPathFinder pf = new DijkstraShortestPathFinder(g, sN, weighter);
+		pf.calculate();
+		return pf.getPath(dN);
+	}
+
+	//lon,lat
+	private Coordinate getPosition(Node n){
+		if(n==null) return null;
+		Point pt = (Point)n.getObject();
+		if(pt==null) return null;
+		return pt.getCoordinate();
+	}
+
+	//get closest node from a position (lon,lat)
+	public Node getNode(Coordinate c){
+		double dMin = Double.MAX_VALUE;
+		Node nMin=null;
+		for(Object o : g.getNodes()){
+			Node n = (Node)o;
+			double d = getPosition(n).distance(c); //TODO fix that !
+			//double d=Utils.getDistance(getPosition(n), c);
+			if(d==0) return n;
+			if(d<dMin) {dMin=d; nMin=n;}
+		}
+		return nMin;
+	}
+
+	//return the distance in km. Distance to closest node
+	private double getDistanceToNetworkKM(Coordinate c) {
+		return Utils.getDistance(c, getPosition(getNode(c)));
+	}
+
+
+	//return the route geometry from origin/destination coordinates
+	public Feature getRoute(double oLon, double oLat, double dLon, double dLat) {
+		return getRoute(new Coordinate(oLon,oLat), new Coordinate(dLon,dLat));
+	}
+	public Feature getRoute(Coordinate oPos, Coordinate dPos) {
+		return getRoute(oPos, getNode(oPos), dPos, getNode(dPos));
+	}
+	//get the route when the node are known
+	public Feature getRoute(Coordinate oPos, Node oN, Coordinate dPos, Node dN) {
+		GeometryFactory gf = new GeometryFactory();
+
+		//get node positions
+		Coordinate oNPos = getPosition(oN), dNPos = getPosition(dN);
+
+		//test if route should be based on network
+		//route do not need network if straight line between two points is smaller than the total distance to reach the network
+		double dist = -1;
+		dist = Utils.getDistance(oPos, dPos);
+		double distN = -1;
+		distN = Utils.getDistance(oPos, oNPos) + Utils.getDistance(dPos, dNPos);
+
+		if(dist>=0 && distN>=0 && distN > dist){
+			//return direct route
+			Feature rf = new Feature();
+			rf.setGeom( gf.createMultiLineString(new LineString[]{ gf.createLineString(new Coordinate[]{oPos,dPos}) }) );
+			return rf;
+		}
+
+		//Compute dijkstra from start node
+		Path path = null;
+		synchronized (g) {
+			path = getShortestPath(g, oN,dN,weighter);
+		}
+
+
+		if(path == null) {
+			Feature rf = new Feature();
+			rf.setGeom(null);
+			return rf;
+		}
+
+		//build line geometry
+		LineMerger lm = new LineMerger();
+		for(Object o : path.getEdges()){
+			Edge e = (Edge)o;
+			SimpleFeature f = (SimpleFeature) e.getObject();
+			if(f==null) continue;
+			Geometry mls = (Geometry)f.getDefaultGeometry();
+			lm.add(mls);
+		}
+		//add first and last parts
+		lm.add(gf.createLineString(new Coordinate[]{oPos,oNPos}));
+		lm.add(gf.createLineString(new Coordinate[]{dPos,dNPos}));
+
+		Collection<?> lss = lm.getMergedLineStrings();
+		Feature rf = new Feature();
+		rf.setGeom( gf.createMultiLineString( lss.toArray(new LineString[lss.size()]) ) );
+		rf.getProperties().put("dFromKM", Utils.getDistance(oPos, oNPos));
+		rf.getProperties().put("dToKM", Utils.getDistance(dPos, dNPos));
+		return rf;
+	}
+
+
+
+	public Collection<Feature> filterPorts(Collection<Feature> ports, double minDistToNetworkKM) {
+		Collection<Feature> pls = new HashSet<Feature>();
+		for(Feature p : ports)
+			if(getDistanceToNetworkKM(p.getGeom().getCoordinate()) <= minDistToNetworkKM)
+				pls.add(p);
+		return pls;
+	}
+
+
+	public Collection<Feature> getRoutes(Collection<Feature> ports, String idProp) {
+		if(idProp == null) idProp = "ID";
+
+		List<Feature> portsL = new ArrayList<Feature>(); portsL.addAll(ports);
+		int nb=portsL.size(); nb=(nb*(nb-1))/2; int cnt=0;
+
+		HashSet<Feature> srs = new HashSet<Feature>();
+		for(int i=0; i<portsL.size(); i++) {
+			Feature pi = portsL.get(i);
+			for(int j=i+1; j<portsL.size(); j++) {
+				Feature pj = portsL.get(j);
+				System.out.println(pi.getProperties().get(idProp) + " - " + pj.getProperties().get(idProp) + " - " + (100*(cnt++)/nb) + "%");
+				Feature sr = getRoute(pi.getGeom().getCoordinate(), pj.getGeom().getCoordinate());
+				Geometry geom = sr.getGeom();
+				sr.getProperties().put("dkm", geom==null? -1 : Utils.getLengthGeo(geom));
+				sr.getProperties().put("from", pi.getProperties().get(idProp));
+				sr.getProperties().put("to", pj.getProperties().get(idProp));
+				srs.add(sr);
+			}
+		}
+		return srs;
+	}
+
+}
