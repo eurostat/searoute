8e541ef68144cfe25d4d4233428cec52952b5da1
jgaffuri <jgaf@protonmail.com>
2021-08-31 16:06:45 +0200

    prepare tests

---
+++ modules/war/src/test/java/eu/europa/ec/eurostat/searoute/SeaRouteWSTest.java
@@ -0,0 +1,16 @@
+package eu.europa.ec.eurostat.searoute;
+
+
+import junit.framework.TestCase;
+
+public class SeaRouteWSTest extends TestCase {
+
+	public static void main(String[] args) throws Exception {
+		junit.textui.TestRunner.run(SeaRouteWSTest.class);
+	}
+
+	public void test() {
+		//TODO
+	}
+
+}
